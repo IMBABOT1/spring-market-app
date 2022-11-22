758e6a0c0680f9ef42c283dc19b3caeb18a2d143
unknown <zlovred12@gmail.com>
2022-11-22 19:16:36 +0300

    temp

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -11,47 +11,9 @@ angular.module('app', []).controller('indexController', function ($scope, $http)
                 max_price: $scope.filter ? $scope.filter.max_price : null
             }
         }).then(function (response) {
-            $scope.ProductsList = response.data.content;
+            $scope.ProductsPage = response.data;
         });
     };
-
-
-
-    $scope.filter = function () {
-        $http({
-            url: contextPath + '/products/score_between',
-            method: 'post',
-            params: {
-                 min: $scope.filtr.min,
-                 max: $scope.filtr.max
-            }
-        }).then(function (response) {
-            $scope.ProductsList = response.data;
-        });
-    }
-
-    $scope.deleteProduct = function (productId) {
-        $http.get(contextPath + '/products/delete/' + productId)
-            .then(function (response) {
-                $scope.loadProducts();
-            });
-    }
-
-    $scope.changePrice = function (productId, delta) {
-        $http({
-            url: contextPath + '/products/change_price',
-            method: 'GET',
-            params: {
-                productId: productId,
-                delta: delta
-            }
-        }).then(function (response) {
-            $scope.loadProducts();
-        });
-    }
-
-
-
 
     $scope.loadProducts();
 });
